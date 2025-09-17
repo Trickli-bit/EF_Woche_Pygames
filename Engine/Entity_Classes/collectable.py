d02f0a3b7e1a7a4e243d9c36b2006c7cd9b63d46
Gamdschie <aiko.baumgartner@gmx.ch>
2025-09-17 21:34:12 +0200

    Inventory für Tools (vertical)

--- Engine/Entity_Classes/collectable.py
+++ Engine/Entity_Classes/collectable.py
@@ -21,6 +21,7 @@ class Collectable(entities.Entity):
         self.collected = False
         self.pick_up = None
         self.source = source
+        self.function = "Item"
         print("getting created")
 
 
@@ -70,6 +71,7 @@ class Stick(Collectable):
             ani_animations=ani_animations
         )
         self.name = "Stick"
+        self.function = "Item"
         self.value = 1
 
 class Rock(Collectable):
@@ -84,6 +86,7 @@ class Rock(Collectable):
             ani_animations=ani_animations
         )
         self.name = "Rock"
+        self.function = "Item"
         self.value = 1
 
 class Mushroom_juice(Collectable):
@@ -98,4 +101,20 @@ class Mushroom_juice(Collectable):
             ani_animations=ani_animations
         )
         self.name = "Mushroom_juice"
+        self.function = "Item"
         self.value = 1   
+
+class Axe(Collectable):
+    def __init__(self, pos_x, pos_y, rect=pygame.Rect(0,0,64,64), rect_attach="topleft",
+                 scale=(64,64), source=r"Engine\Entity_Classes\Sprites_Entity_Classes\Axe.png",
+                 solid=False, is_spritesheet=False, fix=False,
+                 base_sprite=0, ani_frames_count=0, ani_animations={}):
+        super().__init__(
+            pos_x=pos_x, pos_y=pos_y, rect=rect, rect_attach=rect_attach,
+            scale=scale, source=source, solid=solid, is_spritesheet=is_spritesheet,
+            fix=fix, base_sprite=base_sprite, ani_frames_count=ani_frames_count,
+            ani_animations=ani_animations
+        )
+        self.name = "Axe"
+        self.function = "Tool"
+        self.value = 1
--- main.py
+++ main.py
@@ -142,7 +142,7 @@ while running:
         overlayGroup.update(-Player.dx, -Player.dy, keys,)
         overlayGroup.draw(screen)
 
-        overlayGroup = generation.updateToolbar()
+        overlayGroup = generation.updateInventory()
 
 
         if Player.rect.colliderect(Axecrafter.rect) and Axecrafter.has_tool == False:
