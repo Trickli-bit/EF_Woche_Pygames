951e8fde60eec1d704c6ed3dacd46f32b454624e
Trickli-bit <timon.rickli@students.lerbermatt.ch>
2025-09-18 23:10:21 +0200

    correctedLaser

--- main.py
+++ main.py
@@ -138,6 +138,8 @@ while running:
         start_generation = False
 
     if maingame:
+        if trigger is not None:
+            print(trigger.x, trigger.y )
 
 
         floor_group.update(-Player.dx, -Player.dy, keys)
@@ -206,7 +208,6 @@ while running:
         overlayGroup = generation.updateInventory()
 
         if trigger is not None:
-            print(trigger)
             if trigger.colliderect(Player.rect):
                 game_finished = True
 
